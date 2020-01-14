d55d28a643f10123933faa1911642f8ef1c7f22e
FrankieBoye <frankie4@btinternet.com>
2020-01-14 22:20:42 +0000

    added drivetrain options

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  state = {
+    drivetrain: '',
+  }
+
+drivetrain = (e) => {
+  this.setState({
+    drivetrain: e.target.value,
+  })
+  console.log(e.target.value)
+}
+
   render() {
     return (
       <div
-      class="header">
+      className="header">
       <h1>Performance Calculator</h1>
-      <p>Coming soon...</p>
+      <p>Select drivetrain</p>
+      <button onClick={this.drivetrain} value="RWD">RWD</button>
+      <button onClick={this.drivetrain} value="FWD">FWD</button>
+      <button onClick={this.drivetrain} value="AWD">AWD</button>
       </div>
     );
   }
