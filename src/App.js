a2fa7a38f66c0d6eb3112d55bf19afbd4c20ecac
FrankieBoye <frankie4@btinternet.com>
2020-01-16 22:12:11 +0000

    added kerb weight input field

--- src/App.js
+++ src/App.js
@@ -16,11 +16,18 @@ drivetrain = (e) => {
       <div
       className="header">
       <h1>Performance Calculator</h1>
-      <p>Select drivetrain</p>
       <button onClick={this.drivetrain} value="RWD">RWD</button>
       <button onClick={this.drivetrain} value="FWD">FWD</button>
       <button onClick={this.drivetrain} value="AWD">AWD</button>
+
+      <form onSubmit={this.handleSubmit}>
+       <label htmlFor="weight"><font size="3">kerb weight</font> </label>
+       <input id="weight" name="weight" type="text" />
+       <button>enter</button>
+     </form>
+
       </div>
+
     );
   }
 }
