a889002f329991b6afd4879ca55f1470739eeca8
FrankieBoye <frankie4@btinternet.com>
2020-02-03 14:44:46 +0000

    code added for 0-100 calculations

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     weight: '',
     bhp: '',
   }
-  this.calculate = this.calculate.bind(this);
+  this.calculate0to60 = this.calculate0to60.bind(this);
+  this.calculate0to100 = this.calculate0to100.bind(this);
 }
 
 drivetrain = (e) => {
@@ -29,11 +30,24 @@ drivetrain = (e) => {
     },()=>console.log(this.state.bhp));
   }
 
-  calculate() {
+  calculate0to60() {
     var x = 0
       if(this.state.drivetrain==='RWD') x=+0.17
       if(this.state.drivetrain==='FWD') x=+0.69
       if(this.state.drivetrain==='AWD') x=-0.39
+
+    var y = ((this.state.weight / (this.state.bhp * 0.8))+x).toFixed(1)
+    if(isNaN(y)) return '---'
+    if(y == Infinity) return '---'
+    return y
+    console.log(y)
+  }
+
+  calculate0to100() {
+    var x = 0
+      if(this.state.drivetrain==='RWD') x=+4.87
+      if(this.state.drivetrain==='FWD') x=+5.39
+      if(this.state.drivetrain==='AWD') x=+5.19
 
     var y = ((this.state.weight / (this.state.bhp * 0.8))+x).toFixed(1)
     if(isNaN(y)) return '---'
@@ -50,7 +64,7 @@ drivetrain = (e) => {
       <div
       className="header">
       <h1>Performance Calculator</h1>
-      <input type="button" onClick={this.drivetrain} class="active" value="RWD"/>
+      <input type="button" onClick={this.drivetrain} value="RWD"/>
       <input type="button" onClick={this.drivetrain} value="FWD"/>
       <input type="button" onClick={this.drivetrain} value="AWD"/>
 
@@ -62,7 +76,8 @@ drivetrain = (e) => {
             <input type="number" value={this.state.value} onChange={this.bhp} />
       </form>
       <br></br>
-      <div>0-60 = {this.calculate()} seconds</div>
+      <div>0-60 = {this.calculate0to60()} seconds</div>
+      <div>0-100 = {this.calculate0to100()} seconds</div>
       </div>
 
     );
